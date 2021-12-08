ab5fe243cf1dceae8f42cdb00037c10e9cf2d888
asus <asus@DESKTOP-KBBQBNP>
2021-12-08 23:18:33 +0330

    Commit add mongo ConnectionString and CatalogInter

---
+++ Src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Configuration;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DataBaseSetting:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DataBaseSettings:DataBaseName"));
+
+            GetAllProducts = database.GetCollection<Product>(configuration.GetValue<string>("DataBaseSettings:CollectionName"));
+        }
+
+        public IMongoCollection<Product> GetAllProducts { get; }
+    }
+}
