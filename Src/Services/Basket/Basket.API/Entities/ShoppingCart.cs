52cefd7d4bdda842617838e1c6fcc147e6b729d5
asus <asus@DESKTOP-KBBQBNP>
2021-12-23 23:07:11 +0330

    Create Entities Basket

---
+++ Src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+
+        }
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+        public string UserName { get; set; }
+
+        public List<ShoppingCartItems> ItemsCart { get; set; } = new List<ShoppingCartItems>();
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalprice = 0;
+                foreach (var item in ItemsCart)
+                {
+                    totalprice += item.Price * item.Quantity;
+                }
+
+                return totalprice;
+            }
+        }
+    }
+
+}
--- Src/Services/Basket/Basket.API/Startup.cs
+++ Src/Services/Basket/Basket.API/Startup.cs
@@ -25,6 +25,14 @@ namespace Basket.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            #region Redis connecting
+
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = "localhost:6379";
+            });
+
+            #endregion
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
